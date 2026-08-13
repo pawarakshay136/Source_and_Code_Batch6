f78c317ad6edeaab970d576ee02ad1cda3511489
pawarakshay136 <pawarakshay136@gmail.com>
2026-08-13 18:31:12 +0530

    demo_2 lecture: 12th August 2026 Wednesday

---
+++ demo_2.js
@@ -0,0 +1,69 @@
+// 12th August 2026 Wednesday
+
+// Variables in JS
+
+// Variable = data container used to store value
+
+// 1. let = used when the value in the data container can be updated as required
+
+let num1 = 10
+console.log(num1)
+
+let name = "Akshay"
+console.log("Name is :- " + name)
+
+let fullName = "Akshay A Pawar"
+console.log("Full Name is :- " + fullName)
+
+// 2. var = used when the value in data container can be updated as required ( old way to declare variable)
+
+var x = 12
+console.log(x)
+
+var className = "Source and Code"
+console.log(className)
+
+// 3. const = Value cannot be updated
+
+const pi = 3.1432
+console.log("Value of pi is = "+ pi)
+
+// pi = 12
+// console.log(pi)  = TypeError: Assignment to constant variable.
+
+const name1 = "Akshay"
+console.log(name1)
+
+
+/*
+STANDARD TO DECLARE VARIABLES
+
+
+Code Quality:
+Writing clean, readable, and meaningful code improves code quality
+
+Rule 1:
+Variable names should be unique
+Do not declare two variables with the same name in the same scope
+
+Rule 2:
+A variable name should not start with a number or a special character
+
+Rule 3:
+You cannot use special characters in variable names
+Only underscore (_) and dollar sign ($) are allowed
+
+Rule 4:
+Variable names should reflect the value stored in the variable
+Meaningful names improve code quality and readability
+
+Rule 5:
+JavaScript is a case-sensitive language
+Variable names with different letter cases are treated as different names
+Example: name, Name, and NAME are different
+
+Rule 6:
+Variable names should follow camelCase convention
+Example: firstName, fullName, mobileNumber, emailAddress
+
+*/
