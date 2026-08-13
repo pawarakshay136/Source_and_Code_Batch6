73204d8a7ccf21982a1b3bd3991812ca5477f757
pawarakshay136 <pawarakshay136@gmail.com>
2026-08-13 17:51:38 +0530

    demo_1 assignment

---
+++ demo_1.js
@@ -0,0 +1,18 @@
+
+console.log("Akshay Pawar")
+console.log("Welcome to Source and Code Batch 6")
+console.log("Playwright Automation Testing")
+
+
+// Comments:
+// 1. Single-line comments
+// 2. Multiple-line comments
+
+// Comments help developers understand what the code is doing
+
+
+/*
+Name: Akshay Pawar
+From: Tuljapur
+Skills: Manual Testing, Java Selenium Automation Testing
+*/ 
