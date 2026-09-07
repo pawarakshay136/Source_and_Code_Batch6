5365f1d8946b4c9270e8f06f4c227695f93b9905
pawarakshay136 <pawarakshay136@gmail.com>
2026-09-08 01:13:28 +0530

    Day01 JavaScript Variables Assignment 07th September 2026, Monday

---
+++ JavaScript/Assignments/Day01_Variables_Assignment.js
@@ -0,0 +1,101 @@
+//Q1 --> you have write a code to print your name in the terminal
+
+let fullName = "Akshay Pawar"
+console.log(fullName)
+
+
+
+// Write a single line comment which says, comments can make code readable
+
+// Comments can make code readable
+
+
+
+
+// Write another single comment which says, Welcome to JavaScript
+
+// Welcome to JavaScript
+
+
+
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/* 
+
+Comments can make code readable,
+easy to reuse and informative
+
+*/
+
+
+
+
+// Declare four variables without assigning values
+
+let x
+let time
+let speed
+let email
+
+
+
+
+
+// Declare four variables with assigned value
+
+let className = "Source and Code"
+let language = "JavaScript"
+let city = "Pune"
+let batchNo = 6
+let isStudent = true
+
+const capital = "New Delhi"
+const state = "Maharashtra"
+const countryCode = "+91"
+const currencyCode = "INR"
+const gstRate = 18
+
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = "Akshay"
+let lastName = "Pawar"
+let maritalStatus = "Single"
+let myCountry = "India"
+let age = 32
+
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+
+let firstName1 = "Akshay", lastName1 = "Pawar", maritalStatus1 = "Single", country1 = "India", age1 = 32;
+
+
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+
+
+let myAge = 32
+let yourAge = 28
+
+console.log("My age:-", myAge)
+console.log("Your age:-", yourAge)
+
+
+
+// ============================================
+
+// Variable declare and reassign the value
+
+// Declaring a variable 
+let currentCity = "Pune" 
+// Printing the initial value of currentCity
+console.log(currentCity)
+// Reassigning a new value to currentCity
+currentCity = "Tuljapur"
+// Printing the updated value of currentCity
+console.log("Now Current City is:-" + " " +currentCity)
