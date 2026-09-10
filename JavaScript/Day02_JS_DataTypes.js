70646fc306162326cc45b8121d29cc1952a19716
pawarakshay136 <pawarakshay136@gmail.com>
2026-09-10 11:32:39 +0530

    Day02 JavaScript DataTypes - 08th September 2026, Tuesday

---
+++ JavaScript/Day02_JS_DataTypes.js
@@ -0,0 +1,51 @@
+// Day02 - 08th September 2026, Tuesday
+
+// DATA STRUCTURES  --> DS 
+
+
+// DATA TYPE JS ->  
+/**
+ * IN JS THERE TWO MAJOR CATAGORY DATA 
+ * 
+ * 1 PRIMIRTIVE DATATYPE   -> data types which are simple in nature and are non changeable (immutable)      
+ * 2 NON PRIMITIVE DATATYPE -> the datatype which are complex in nature and change be change accroding to requrimnet
+ */
+
+// PRIMIRTIVE DATATYPE
+
+// PRIMITIVE DATA INCULDES  
+// 1 NUMBERS -> 
+// 2 STRING -> 
+// 3 BOOLEAN ->
+// 4 undefined-> 
+// 5 null   ->
+
+
+// NON PRIMITIVE -> 
+// 1 ARRAY 
+// 2 OBJECT 
+
+
+
+// HOW TO CHECK A DATA TYPE 
+
+let Number_1 = 1
+
+let Name = "siddhant"
+
+let value = true
+
+let token 
+
+let v1 = null
+
+
+// you will use a opertor called type of 
+
+console.log(typeof Number_1)
+
+console.log(typeof value)
+
+console.log( typeof token )
+
+console.log( typeof v1) // bug in js 
